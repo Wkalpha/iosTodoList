aba19e98d29eb00c3cf57ddc488ec68e442abd65
Wkalpha <wayne1444@gmail.com>
2019-04-16 21:27:17 +0800

    Add sqlite and insert sql

--- iosTodoList/ViewController.swift
+++ iosTodoList/ViewController.swift
@@ -7,27 +7,56 @@
 //
 
 import UIKit
+import SQLite
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var textField: UITextField!
     
+    var myTask:[String] = []
     @IBAction func addBtn(_ sender: UIButton) {
         // Check text if empty pop up alert window and return 0 do nothing
         if(textField.text == ""){
-            checkData()
+            // show alert window
+            // see line 32
+            showAlert()
             return
         }
-        // Add user input text
-        myTask.append(textField.text!)
         
-        // Reload when press add button
+        do{
+            let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+            let db = try? Connection("\(path)/todo.sqlite")
+            
+            let main = Table("main")
+            let id = Expression<Int64>("id")
+            let text = Expression<String>("text")
+            
+            try! db?.run(main.create(ifNotExists: true,block:{(table) in
+                table.column(id, primaryKey: true)
+                table.column(text)}))
+            
+            do {
+                let rowid = try db?.run(main.insert(text <- textField.text!))
+                myTask = []
+                for toDoList  in (try? db?.prepare(main))!! {
+                    myTask.append("\(toDoList[text])")
+                }
+                print("inserted id: \(rowid!)")
+            } catch {
+                print("insertion failed: \(error)")
+            }
+            
+        }catch{
+            print(error)
+        }
+        
         tableView.reloadData()
+        
         // Clean text
         textField.text = ""
     }
     
-    func checkData() {
+    func showAlert() {
         let alertController = UIAlertController(title: "Text can't be empty", message: "", preferredStyle: UIAlertController.Style.alert)
         let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
         alertController.addAction(okAction)
@@ -35,8 +64,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         present(alertController, animated: true, completion: nil)
     }
     
-    var myTask:[String] = []
-    
+    // 底線_ 代表可以省略參數名稱直接使用
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return myTask.count
     }
@@ -52,9 +80,32 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // The app startup view
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         tableView.dataSource = self
         tableView.delegate = self
+        do{
+            let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+            let db = try? Connection("\(path)/todo.sqlite")
+            
+            let main = Table("main")
+            let id = Expression<Int64>("id")
+            let text = Expression<String>("text")
+            
+            try! db?.run(main.create(ifNotExists: true,block:{(table) in
+                table.column(id, primaryKey: true)
+                table.column(text)}))
+            
+            do {
+                myTask = []
+                for toDoList  in (try? db?.prepare(main))!! {
+                    myTask.append("\(toDoList[text])")
+                }
+            } catch {
+                print(error)
+            }
+            
+        }catch{
+            print(error)
+        }
     }
 
 
